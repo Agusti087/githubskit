95c3ad83341334c7c0b3456e9bab86d5b56bfba5
Agusti087 <oskar@kullgren.org>
2025-10-21 11:57:16 +0200

    fixed bullet cooldown

--- Assets/CreateBullet.cs
+++ Assets/CreateBullet.cs
@@ -19,9 +19,8 @@ public class CreateBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Cooldowntime -= Time.deltaTime;
 
-        if (Input.GetKeyDown(Shoot) && Cooldowntime == 0)
+        if (Input.GetKeyDown(Shoot) && Cooldowntime <= 0)
         {
             print("Jag skjuter dig");
             Instantiate(Bullet,rightPoint.position, Quaternion.identity);
@@ -33,9 +32,10 @@ public class CreateBullet : MonoBehaviour
         
         Bullet.transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
 
-        if (Cooldowntime <= 0)
+        if (Cooldowntime > 0)
         {
-            Cooldowntime = 0;
+
+            Cooldowntime -= Time.deltaTime;
         }
     }
     
