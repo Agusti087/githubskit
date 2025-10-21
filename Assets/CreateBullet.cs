e4e30b828912d34c85e55578835b52928d7f407d
Agusti087 <oskar@kullgren.org>
2025-10-21 10:51:04 +0200

    added player and gave movement

---
+++ Assets/CreateBullet.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CreateBullet : MonoBehaviour
+{
+    [SerializeField] KeyCode Shoot = KeyCode.Return;
+    public GameObject Bullet;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(Shoot))
+        {
+            print("Jag skjuter dig");
+            Instantiate(Bullet);
+        }
+    }
+    
+}
